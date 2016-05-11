ce358c5e17c640742dcbfd9a095264cea8f7bdf7
Tamás Perlaki <pt1029@gmail.com>
2016-05-11 22:04:03 +0200

    Added Grunt with jshint, package.json, beep producer for karma

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports = function(grunt) {
+	var jshintFiles = ['Gruntfile.js',
+	                   'src/main/resources/static/js/**/*.js',
+	                   'src/test/resources/js/**/*.js',
+	                   '!src/main/resources/static/js/3rdparty/*.js',
+	                   '!src/test/resources/js/3rdparty/*.js'
+	                   ];
+	
+    grunt.initConfig({
+    	watch: {
+			files: jshintFiles,
+		    tasks: ['jshint']
+		},
+        jshint: {
+            files: jshintFiles
+        },
+        karma: {
+        	options: {
+        		configFile: 'src/test/resources/karma.conf.js'
+        	},
+        	continuous: {
+        		background: true
+        	}
+        }
+    });
+    
+    grunt.registerTask('default', ['watch']);
+    
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+};
