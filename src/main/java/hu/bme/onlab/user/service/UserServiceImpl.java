e90599616cd66d26c9c7a89ef5ffba71aa31c80a
Tóth Csaba <csaba9308@gmail.com>
2016-11-23 00:54:49 +0100

    Unused dependencies and constants removed from UserServiceImpl class

--- src/main/java/hu/bme/onlab/user/service/UserServiceImpl.java
+++ src/main/java/hu/bme/onlab/user/service/UserServiceImpl.java
@@ -3,23 +3,15 @@ package hu.bme.onlab.user.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.collections.IteratorUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.CrossOrigin;
-
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
-
 import hu.bme.onlab.user.bean.CheckEmailRequest;
 import hu.bme.onlab.user.bean.CheckEmailResponse;
 import hu.bme.onlab.user.bean.PasswordChangeRequest;
 import hu.bme.onlab.user.bean.RoleDTO;
 import hu.bme.onlab.user.bean.SignupRequest;
-import hu.bme.onlab.user.domain.Authority;
 import hu.bme.onlab.user.domain.Role;
 import hu.bme.onlab.user.domain.User;
 import hu.bme.onlab.user.repository.AuthorityRepository;
@@ -29,8 +21,6 @@ import hu.bme.onlab.user.repository.UserRepository;
 @Service
 @Transactional
 public class UserServiceImpl implements UserService {
-
-	private static final String ROLE_USER = "ROLE_USER";
 	
 	private UserRepository userRepository;
 	
